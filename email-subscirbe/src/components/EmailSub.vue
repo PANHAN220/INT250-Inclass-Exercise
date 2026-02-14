01fd484764d18d76740f07ac2acf19c7f35209b3
sakdichot <sakdichot.hanr@mail.kmutt.ac.th>
2026-02-14 13:52:35 +0700

    chore: update padding input

--- email-subscirbe/src/components/EmailSub.vue
+++ email-subscirbe/src/components/EmailSub.vue
@@ -18,10 +18,10 @@
                     <p class="max-w-xs text-xs text-center leading-5 tracking-wide md:text-left">Eat better and exercise better Sign up for the Diet&fitness newsletter</p>
 
                     <div class="flex flex-col mt-5 space-y-4 md:space-x-3 md:flex-row md:space-y-0">
-                        <input type="text" placeholder="Enter your email Address" class="p-2 px-4 text-center
+                        <input type="text" placeholder="Enter your email Address" class="p-2 px-6 text-center
                         text-white bg-zinc-800 border border-zinc-600 placeholder:text-xs placeholder:text-center
                         md:text-left placeholder:md:text-left focus:outline-none"/>
-                        <button class="text-zinc-700 bg-lime-500 hover:bg-lime-700 rounded-md hover:text-white  duration-500 ml-2">
+                        <button class="text-zinc-700 bg-lime-500 hover:bg-lime-700 rounded-md hover:text-white  duration-500 ">
                             <p class="text-xs m-2">Subscribe</p>
                         </button>
                     </div>
