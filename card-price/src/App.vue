a2f06d689053ed0cb20d3897721b6d839a263e5a
sakdichot <sakdichot.hanr@mail.kmutt.ac.th>
2026-03-03 05:07:37 +0700

    fix: update folder name

---
+++ card-price/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Cards from './components/Cards.vue';
+
+const plans = [
+    {
+        title: "Basic",
+        storage: "100GB",
+        price: "1.99",
+        features: [
+            "100GB storage",
+            "Option to add members",
+            "Extra memeber benefits"
+        ]
+    },
+    {
+        title: "Standard",
+        storage: "200GB",
+        price: "3.99",
+        features: [
+            "200GB storage",
+            "Option to add members",
+            "Extra memeber benefits"
+        ]
+    },
+    {
+        title: "Premium",
+        storage: "2TB",
+        price: "8.99",
+        features: [
+            "2TB storage",
+            "Option to add members",
+            "Extra memeber benefits"
+        ]
+    }
+]
+</script>
+
+<template>
+    <div class="bg-[#1E293B] min-h-screen flex items-center justify-center gap-8">
+        <Cards 
+            v-for="plan in plans"
+            :key="plan.title"
+            v-bind="plan"    
+        />
+        
+    </div>
+</template>
+
+<style scoped></style>
---
+++ card-price/src/components/Cards.vue
@@ -0,0 +1,50 @@
+<script setup>
+ defineProps({
+    title: String,
+    storage: String,
+    price: Number,
+    features: Array
+ })
+
+</script>
+
+<template>
+    <!-- Card -->
+    <div class="group bg-[#1E293B] w-64 rounded-lg p-6 text-white shadow-lg border-8 border-gray-500 hover:border-violet-500 transition">
+        
+        <!-- Title -->
+        <div class="text-center text-sm mb-4">
+            <h3>{{title}}</h3>
+        </div>
+
+
+        <!-- Storage -->
+        <div class="text-center text-4xl mb-4 font-serif">
+            <h1>{{storage}}</h1>
+        </div>
+
+        <!-- Prize -->
+        <div class="text-center text-sm mb-6">
+            <h3>${{ price }}/month</h3>
+        </div>
+
+        <!-- Purchase -->
+        <div class="mb-10 ">
+            <button class="block mx-auto w-40 border-2 border-violet-500 py-1.5 rounded-lg group-hover:bg-violet-500 transition ">Purchase</button>
+        </div>
+
+        <hr class="my-6 border-gray-600 -mx-6">
+
+        <!-- Feature -->
+        <ul class="text-center space-y-1">
+            <li v-for="(item, index) in features" :key="index">
+                ✓ {{ item }}
+            </li>
+            
+        </ul>
+
+    </div>
+
+</template>
+
+<style scoped></style>
